93d8a4df5080469f5d127acb94c57eba84074bba
se-ize <70623290+se-ize@users.noreply.github.com>
2023-04-07 02:43:19 +0900

    Time: 19 ms (62.89%), Space: 13.7 MB (36.17%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        for i in s:
+            if i == "{":
+                stack.append("}")
+            elif i == "(":
+                stack.append(")")
+            elif i == "[":
+                stack.append("]")
+            elif not stack or stack.pop() != i:
+                return False
+        return not stack
+        
